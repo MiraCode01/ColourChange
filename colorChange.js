41bccb48fddb3f52e442f93f1eb16232450ece3d
Miracle Omoyowho <89919216+MiraCode01@users.noreply.github.com>
2024-05-08 12:15:56 +0100

    Add files via upload

---
+++ colorChange.js
@@ -0,0 +1,34 @@
+const Back = document.getElementById("circle")
+// Back.style.backgroundColor = 'purple'
+
+function Red(){
+    Back.style.backgroundColor = 'red'
+}
+function Blue(){
+    Back.style.backgroundColor = 'blue'
+}
+function Green(){
+    Back.style.backgroundColor = 'green'
+}
+function Purple(){
+    Back.style.backgroundColor = 'purple'
+}
+function Yellow(){
+    Back.style.backgroundColor = 'yellow'
+}
+function Pink(){
+    Back.style.backgroundColor = 'pink'
+}
+function Grey(){
+    Back.style.backgroundColor = 'grey'
+}
+function Black(){
+    Back.style.backgroundColor = 'black'
+}
+function Orange(){
+    Back.style.backgroundColor = 'orange'
+}
+function Brown(){
+    Back.style.backgroundColor = 'brown'
+}
+
